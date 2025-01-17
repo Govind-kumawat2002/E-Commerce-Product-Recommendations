a9bdae4ae2612146fa4b3e1e4159c7169701c207
govind-kumawat2002 <kumawatritik54@gmail.com>
2025-01-17 18:21:53 +0530

    backend and frond of our project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask ,render_template ,request ,url_for
+import joblib
+import pandas as pd 
+import numpy as np 
+main=Flask(__name__)
+@main.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/bookName')
+def show_book_name():
+    book_name = list(pd.read_csv('dataset/All Electronics.csv')['Book-Title'].values)
+    return render_template("bookname.html",list_fo_book_name = book_name)
+# to get the popularity based recommandation system
+
+
+if __name__=="__main__":
+    main.run(debug= True)
